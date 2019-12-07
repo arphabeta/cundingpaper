e7e1c97b08082b1fa65f27d8760116fbcfeb8c71
yaju <50805764+arphabeta@users.noreply.github.com>
2019-12-08 00:58:27 +0900

    Add files via upload
    change list
    1. <select>tag -> <ul> tag.  because select tag not work in CSS on <option>tag
    2. JS, CSS Due to changed <ul> tag
    3.  z-index works !

--- js/js.js
+++ js/js.js
@@ -1,98 +1,163 @@
 $(document).ready(function(){
 
-  $("#selectBox").change(function() {
-  	var state = $("#selectBox option:selected").val();
-  	if ( state == "option0" ) {
-  		$(".if").hide();
-      $(".ifs").hide();
-      $(".else").hide();
-  		$(".elseif").hide();
-      $(".while").hide();
-      $(".switch").hide();
-      $(".for").hide();
-      $(".fors").hide();
-  	}
-    if ( state == "option1" ) {
-  		$(".if").show();
-      $(".ifs").hide();
-      $(".else").hide();
-  		$(".elseif").hide();
-      $(".while").hide();
-      $(".switch").hide();
-      $(".for").hide();
-      $(".fors").hide();
-  	}
-    if ( state == "option2" ) {
-      $(".if").hide();
-      $(".ifs").show();
-      $(".else").hide();
-      $(".elseif").hide();
-      $(".while").hide();
-      $(".switch").hide();
-      $(".for").hide();
-      $(".fors").hide();
-    }
-    if ( state == "option3" ) {
-      $(".if").hide();
-      $(".ifs").hide();
-      $(".else").show();
-      $(".elseif").hide();
-      $(".while").hide();
-      $(".switch").hide();
-      $(".for").hide();
-      $(".fors").hide();
-    }
-    if ( state == "option4" ) {
-      $(".if").hide();
-      $(".ifs").hide();
-      $(".else").hide();
-      $(".elseif").show();
-      $(".while").hide();
-      $(".switch").hide();
-      $(".for").hide();
-      $(".fors").hide();
-    }
-    if ( state == "option5" ) {
-      $(".if").hide();
-      $(".ifs").hide();
-      $(".else").hide();
-      $(".elseif").hide();
-      $(".while").show();
-      $(".switch").hide();
-      $(".for").hide();
-      $(".fors").hide();
-    }
-    if ( state == "option6" ) {
-      $(".if").hide();
-      $(".ifs").hide();
-      $(".else").hide();
-      $(".elseif").hide();
-      $(".while").hide();
-      $(".switch").show();
-      $(".for").hide();
-      $(".fors").hide();
-    }
-    if ( state == "option7" ) {
-      $(".if").hide();
-      $(".ifs").hide();
-      $(".else").hide();
-      $(".elseif").hide();
-      $(".while").hide();
-      $(".switch").hide();
-      $(".for").show();
-      $(".fors").hide();
-    }
-    if ( state == "option8" ) {
-      $(".if").hide();
-      $(".ifs").hide();
-      $(".else").hide();
-      $(".elseif").hide();
-      $(".while").hide();
-      $(".switch").hide();
-      $(".for").hide();
-      $(".fors").show();
-    }
-
+  $(".arrow-down").click(function(){
+    // $("#selectBox").text('원하는 문법 선택');
+    $(".arrow-up").show().stop(true,true);
+    $(".arrow-down").hide();
+    $(".off_list").show().stop(true,true);
   });
+  $(".arrow-up").click(function(){
+    $(".arrow-up").hide();
+    $(".arrow-down").show();
+    $(".off_list").hide();
+    $(".section2").hide();
+  });
+
+    $("#selectBox").click(function(){
+        $(".off_list").hide();
+        $(".arrow-up").hide();
+        $(".arrow-down").show();
+        $(".if").hide();
+
+        $(".ifs").hide();
+        $(".else").hide();
+    		$(".elseif").hide();
+        $(".while").hide();
+        $(".switch").hide();
+        $(".for").hide();
+        $(".fors").hide();
+   });
+
+   var num=$(".off_list").index();  //0,1,2.....순서를 매겨줌
+
+    $(".off_list").eq(0).click(function(){
+      $("#selectBox").text('if문');
+      $(".arrow-up").hide();
+      $(".arrow-down").show();
+      $(".off_list").hide();
+
+      $(".if").show().stop(true,true);
+      $(".ifs").hide();
+      $(".else").hide();
+    	$(".elseif").hide();
+      $(".while").hide();
+      $(".switch").hide();
+      $(".for").hide();
+      $(".fors").hide();
+   });
+
+   $(".off_list").eq(1).click(function(){
+     $("#selectBox").text('중첩 if문');
+     $(".arrow-up").hide();
+     $(".arrow-down").show();
+     $(".off_list").hide();
+
+     $(".if").hide();
+     $(".ifs").show().stop(true,true);
+     $(".else").hide();
+     $(".elseif").hide();
+     $(".while").hide();
+     $(".switch").hide();
+     $(".for").hide();
+     $(".fors").hide();
+  });
+
+  $(".off_list").eq(2).click(function(){
+    $("#selectBox").text('else문');
+    $(".arrow-up").hide();
+    $(".arrow-down").show();
+    $(".off_list").hide();
+
+    $(".if").hide();
+    $(".ifs").hide();
+    $(".else").show().stop(true,true);
+    $(".elseif").hide();
+    $(".while").hide();
+    $(".switch").hide();
+    $(".for").hide();
+    $(".fors").hide();
+ });
+
+ $(".off_list").eq(3).click(function(){
+   $("#selectBox").text('else if문');
+   $(".arrow-up").hide();
+   $(".arrow-down").show();
+   $(".off_list").hide();
+
+   $(".if").hide();
+   $(".ifs").hide();
+   $(".else").hide();
+   $(".elseif").show().stop(true,true);
+   $(".while").hide();
+   $(".switch").hide();
+   $(".for").hide();
+   $(".fors").hide();
+});
+
+$(".off_list").eq(4).click(function(){
+  $("#selectBox").text('while문');
+  $(".arrow-up").hide();
+  $(".arrow-down").show();
+  $(".off_list").hide();
+
+  $(".if").hide();
+  $(".ifs").hide();
+  $(".else").hide();
+  $(".elseif").hide();
+  $(".while").show().stop(true,true);
+  $(".switch").hide();
+  $(".for").hide();
+  $(".fors").hide();
+});
+
+$(".off_list").eq(5).click(function(){
+  $("#selectBox").text('switch문');
+  $(".arrow-up").hide();
+  $(".arrow-down").show();
+  $(".off_list").hide();
+
+  $(".if").hide();
+  $(".ifs").hide();
+  $(".else").hide();
+  $(".elseif").hide();
+  $(".while").hide();
+  $(".switch").show().stop(true,true);
+  $(".for").hide();
+  $(".fors").hide();
+});
+
+$(".off_list").eq(6).click(function(){
+  $("#selectBox").text('for문');
+  $(".arrow-up").hide();
+  $(".arrow-down").show();
+  $(".off_list").hide();
+
+  $(".if").hide();
+  $(".ifs").hide();
+  $(".else").hide();
+  $(".elseif").hide();
+  $(".while").hide();
+  $(".switch").hide();
+  $(".for").show().stop(true,true);
+  $(".fors").hide();
+});
+
+$(".off_list").eq(7).click(function(){
+  $("#selectBox").text('중첩 for문');
+  $(".arrow-up").hide();
+  $(".arrow-down").show();
+  $(".off_list").hide();
+
+  $(".if").hide();
+  $(".ifs").hide();
+  $(".else").hide();
+  $(".elseif").hide();
+  $(".while").hide();
+  $(".switch").hide();
+  $(".for").hide();
+  $(".fors").show().stop(true,true);
+});
+
+
 
 });
